e1f85be0f8130a71e6036eb654d3a24d542e0464
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-11-05 22:03:08 +0100

    Init

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Page = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default Page;
