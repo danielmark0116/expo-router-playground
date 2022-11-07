7a505dd1af5115740515e5344b93fd51baf0755b
Daniel Grychtoł <grychtol.daniel@gmail.com>
2022-11-07 12:19:18 +0100

    Update init branch

--- app/register.tsx
+++ app/register.tsx
@@ -1,13 +1,9 @@
 import { View, Text } from "react-native";
 
-const friendsList = ["Joey", "Chandler", "Monica", "Rachel", "Phoebe", "Ross"];
-
 const Page = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      {friendsList.map((friend) => (
-        <Text key={friend}>{friend}</Text>
-      ))}
+      <Text>Register</Text>
     </View>
   );
 };
