eaa988779f8cb3c3ca3713e5da5e1eccfb32cebd
Daniel Grychtoł <grychtol.daniel@gmail.com>
2023-01-19 21:20:47 +0100

    Update friend details screen

--- app/[friend].tsx
+++ app/[friend].tsx
@@ -1,13 +1,11 @@
 import { Text, View } from "react-native";
-import { Stack, useSearchParams } from "expo-router";
+// import { Stack } from "expo-router";
 
 const Page = () => {
-  const { friend } = useSearchParams();
-
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Stack.Screen options={{ title: friend }} />
-      <Text>{friend}</Text>
+      {/* <Stack.Screen /> */}
+      <Text>Friend</Text>
     </View>
   );
 };
