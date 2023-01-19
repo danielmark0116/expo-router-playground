abb7e0bd65a4469d57563ba8a122f51b836f71fd
Daniel Grychtoł <grychtol.daniel@gmail.com>
2023-01-19 21:19:42 +0100

    Update init

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,15 @@
-import { useRouter, Stack } from "expo-router";
+import { Stack } from "expo-router";
 import { View, TouchableOpacity } from "react-native";
 import { Text } from "app/components/Text";
 
 const friendsList = ["Joey", "Chandler", "Monica", "Rachel", "Phoebe", "Ross"];
 
 const Page = () => {
-  const { push } = useRouter();
-
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Stack.Screen options={{ title: "Friends App" }} />
       {friendsList.map((friend) => (
-        <TouchableOpacity
-          key={friend}
-          onPress={() => {
-            push(`/${friend}`);
-          }}
-        >
+        <TouchableOpacity key={friend}>
           <Text>{friend}</Text>
         </TouchableOpacity>
       ))}
