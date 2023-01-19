ae180d56f1e7559df39906d0f8e15683dec36231
Daniel Grychtoł <grychtol.daniel@gmail.com>
2023-01-19 21:22:26 +0100

    Update register screen

--- app/register.tsx
+++ app/register.tsx
@@ -4,9 +4,7 @@ import { Text } from "app/components/Text";
 const Page = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Hello World!</Text>
-
-      <Text>Register your account in order to use the app</Text>
+      <Text>Register</Text>
     </View>
   );
 };
